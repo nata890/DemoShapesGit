dde6ed1b15fd11872cb2da6eaad8fb44b6adeb30
Natalia Ceballos <nataliaceballos1703@gmail.com>
2022-09-22 14:20:51 -0500

    Nueva clase Shape

---
+++ src/main/java/org/example/Shape.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public abstract class Shape {
+    private int x = 0;
+    private int y = 0;
+
+    public int getX() {
+
+        return x;
+    }
+
+    public void setX(int x) {
+
+        this.x = x;
+    }
+
+    public int getY() {
+
+        return y;
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimetro();
+
+}
